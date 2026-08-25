484b2cd85ba2d5443badef616991264209d8a4ac
Ismaiel Sabet <95542909+IsmaielSemo@users.noreply.github.com>
2026-08-25 12:10:44 +0300

    Update run_server.py

--- Phase 3/run_server.py
+++ Phase 3/run_server.py
@@ -6,9 +6,9 @@ import logging
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-    logging.info("Starting production server on port 80...")
+    logging.info("Starting production server on port 5000...")
 
     # 0.0.0.0 tells it to accept connections from ANY computer on the company network
     # Port 80 is the default web port, so users won't have to type a port number in the URL
-    serve(app, host='0.0.0.0', port=80, threads=4)
+    serve(app, host='0.0.0.0', port=5000, threads=4)
 
